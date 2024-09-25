cb2cad05ad5cf5c5ae4445b468ddb2b1e7225c38
mahfujur-shohan <mahfujurrahmancityu@gmail.com>
2024-09-25 23:25:56 +0600

    added controller for employee

---
+++ src/main/java/com/mrshohan/springboot/crud_employee/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.mrshohan.springboot.crud_employee.rest;
+
+import com.mrshohan.springboot.crud_employee.dao.EmployeeDAO;
+import com.mrshohan.springboot.crud_employee.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}
